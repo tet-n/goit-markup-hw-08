1318efac8e151abb4774a01420d9651c8c00fd0c
Tetiana Nivchyk <tata.02od@gmail.com>
2022-12-14 14:29:05 +0200

    added no-scroll for body

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),//add no scroll while modal is open
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('backdrop--is-hidden');
+    refs.body.classList.toggle('no-scroll');//add no scroll while modal is open
   }
 })();
