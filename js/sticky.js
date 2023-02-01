81728a790719d56b3c9177662cfb8d8744db5563
Tetiana Nivchyk <tata.02od@gmail.com>
2023-02-01 12:55:07 +0200

    added sticky navigation

---
+++ js/sticky.js
@@ -0,0 +1,18 @@
+const heroRef = document.querySelector('.hero');
+const headerRef = document.querySelector('.header');
+
+console.log(heroRef);
+const getStickyHeader = function (entries) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) {
+    headerRef.classList.add('sticky');
+  } else {
+    headerRef.classList.remove('sticky');
+  }
+};
+
+const heroObserver = new IntersectionObserver(getStickyHeader, {
+  root: null,
+  threshold: 1,
+});
+heroObserver.observe(heroRef);
