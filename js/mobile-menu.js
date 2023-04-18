6e3834a2d467620b4d698bb4072d45cf4905c4dc
Tetiana Nivchyk <tata.02od@gmail.com>
2023-04-18 19:04:31 +0300

    add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,7 +8,6 @@
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    body.classList.toggle('no-scroll');
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
@@ -26,4 +25,50 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     // bodyScrollLock.enableBodyScroll(document.body);
   });
+})();
+
+(function () {
+  var _overlay = document.querySelector('.js-menu-container');
+  var _clientY = null; // remember Y position on touch start
+
+  _overlay.addEventListener(
+    'touchstart',
+    function (event) {
+      if (event.targetTouches.length === 1) {
+        // detect single touch
+        _clientY = event.targetTouches[0].clientY;
+      }
+    },
+    false
+  );
+
+  _overlay.addEventListener(
+    'touchmove',
+    function (event) {
+      if (event.targetTouches.length === 1) {
+        // detect single touch
+        disableRubberBand(event);
+      }
+    },
+    false
+  );
+
+  function disableRubberBand(event) {
+    var clientY = event.targetTouches[0].clientY - _clientY;
+
+    if (_overlay.scrollTop === 0 && clientY > 0) {
+      // element is at the top of its scroll
+      event.preventDefault();
+    }
+
+    if (isOverlayTotallyScrolled() && clientY < 0) {
+      //element is at the top of its scroll
+      event.preventDefault();
+    }
+  }
+
+  function isOverlayTotallyScrolled() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#Problems_and_solutions
+    return _overlay.scrollHeight - _overlay.scrollTop <= _overlay.clientHeight;
+  }
 })();
