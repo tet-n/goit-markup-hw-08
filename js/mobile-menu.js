042895ce19ad2fb5c6cfb9eebc4d4ae4644e8bc3
Tetiana Nivchyk <tata.02od@gmail.com>
2023-04-18 19:14:42 +0300

    added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,11 +2,11 @@
   const mobileMenu = document.querySelector('.js-menu-container'); //overlay
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  let scrollPos = window.scrollY;
 
   const body = document.querySelector('body');
 
   const toggleMenu = () => {
+    body.classList.toggle('no-scroll');
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
@@ -25,50 +25,4 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     // bodyScrollLock.enableBodyScroll(document.body);
   });
-})();
-
-(function () {
-  var _overlay = document.querySelector('.js-menu-container');
-  var _clientY = null; // remember Y position on touch start
-
-  _overlay.addEventListener(
-    'touchstart',
-    function (event) {
-      if (event.targetTouches.length === 1) {
-        // detect single touch
-        _clientY = event.targetTouches[0].clientY;
-      }
-    },
-    false
-  );
-
-  _overlay.addEventListener(
-    'touchmove',
-    function (event) {
-      if (event.targetTouches.length === 1) {
-        // detect single touch
-        disableRubberBand(event);
-      }
-    },
-    false
-  );
-
-  function disableRubberBand(event) {
-    var clientY = event.targetTouches[0].clientY - _clientY;
-
-    if (_overlay.scrollTop === 0 && clientY > 0) {
-      // element is at the top of its scroll
-      event.preventDefault();
-    }
-
-    if (isOverlayTotallyScrolled() && clientY < 0) {
-      //element is at the top of its scroll
-      event.preventDefault();
-    }
-  }
-
-  function isOverlayTotallyScrolled() {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#Problems_and_solutions
-    return _overlay.scrollHeight - _overlay.scrollTop <= _overlay.clientHeight;
-  }
 })();
